f85f26eca9eda42a45492ffa9c01d07fb727fc65
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2024-08-27 00:40:36 +0200

    Linter fix

--- packages/logger/src/winston-logger.injectable.ts
+++ packages/logger/src/winston-logger.injectable.ts
@@ -10,7 +10,11 @@ export const winstonLoggerInjectable = getInjectable({
   id: "winston-logger",
   instantiate: (di) =>
     createLogger({
-      format: format.combine(format.splat(), format.simple(), format.timestamp({ format: 'DD/MM/YYYY HH:mm:ss' })),
+      format: format.combine(
+        format.splat(),
+        format.simple(),
+        format.timestamp({ format: "DD/MM/YYYY HH:mm:ss" })
+      ),
       transports: di.injectMany(loggerTransportInjectionToken),
     }),
 });
