a11f8a4b17f42729b34a1c877f1d82917d12f356
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2025-02-03 13:49:48 +0100

    Regenerated icons for all OSes

--- freelens/build/icon.sh
+++ freelens/build/icon.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-magick -background none icon.svg -density 300 -resize 408x408 -bordercolor transparent -border 52 icon.png
-magick -background none icon.svg -define icon:auto-resize=16,24,32,48,64,72,96,128,256 icon.ico
+## macOS
+magick -background none icon.svg -density 300 -resize 408x408 -bordercolor transparent -border 52 -verbose icon.png
+
+## Windows
+magick -background none icon.svg -define icon:auto-resize=16,20,24,32,40,48,60,64,72,80,96,256 -verbose icon.ico
+
+## Linux
 for i in 16 22 24 32 36 48 64 72 96 128 192 256 512; do
-	magick icon.png -resize "${i}x${i}" "icons/${i}x${i}.png"
+	border=$(("${i}000" * 51 / 1000000))
+	size=$((i - 2 * border))
+	magick -background none icon.svg -density 300 -resize "${size}x${size}" -bordercolor transparent -border "${border}" -verbose "icons/${i}x${i}.png"
 done
