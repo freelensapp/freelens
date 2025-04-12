fe5d649bb9a810d9458253b4b95cb25e390d769b
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2025-04-12 23:24:57 +0200

    Make linter happy

--- scripts/knip-install-missing-packages.sh
+++ scripts/knip-install-missing-packages.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+set -eo pipefail
 
 pnpm i $(pnpx knip --production --reporter json | jq '.. | objects | select(has("unlisted")) | .unlisted | .[].name' -r | uniq)
 pnpm i -D $(pnpx knip --reporter json | jq '.. | objects | select(has("unlisted")) | .unlisted | .[].name' -r | uniq)
