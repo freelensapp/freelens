abf5dbe59c4a49fe3caf32fa1e230b8c7e32cfaa
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2025-04-13 15:54:06 +0200

    Hotbar test unit fails

--- packages/core/src/features/hotbar/storage/storage-technical.test.ts
+++ packages/core/src/features/hotbar/storage/storage-technical.test.ts
@@ -151,7 +151,7 @@ describe("Hotbars technical tests", () => {
     });
 
     // TODO: By unclear reason, the first hotbar item is not set to welcome page
-    describe.skip("hotbar items", () => {
+    describe("hotbar items", () => {
       it("initially creates default number of empty cells", () => {
         expect(activeHotbar.get()?.items?.length).toEqual(defaultHotbarCells);
       });
