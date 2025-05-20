c58b541047554aab0022463f8477d612e528af77
Guitton Nicolas <nguitton@cegid.com>
2025-05-20 02:08:01 +0200

    Ajout de nouveaux injectables pour la gestion des préférences CRD et mise à jour de la logique de stockage des préférences utilisateur.
    Signed-off-by: Guitton Nicolas <nguitton@cegid.com>

---
+++ packages/core/src/features/preferences/renderer/preference-items/crd/crd-preference-tab.injectable.ts
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) Freelens Authors. All rights reserved.
+ * Copyright (c) OpenLens Authors. All rights reserved.
+ * Licensed under MIT License. See LICENSE in root directory for more information.
+ */
+
+import { getInjectable } from "@ogre-tools/injectable";
+import { preferenceItemInjectionToken } from "../preference-item-injection-token";
+
+const crdPreferenceTabInjectable = getInjectable({
+  id: "crd-preference-tab",
+
+  instantiate: () => ({
+    kind: "tab" as const,
+    id: "crd-tab",
+    parentId: "general-tab-group" as const,
+    pathId: "crd",
+    label: "Crd",
+    orderNumber: 20,
+  }),
+
+  injectionToken: preferenceItemInjectionToken,
+});
+
+export default crdPreferenceTabInjectable;
