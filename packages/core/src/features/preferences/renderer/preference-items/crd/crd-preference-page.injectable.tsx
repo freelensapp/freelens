e6c57af152819fd11b11c2411a3a9f5d1f1dd387
Guitton Nicolas <nguitton@cegid.com>
2025-05-20 02:08:01 +0200

    Ajout de nouveaux injectables pour la gestion des préférences CRD et mise à jour de la logique de stockage des préférences utilisateur.
    Signed-off-by: Guitton Nicolas <nguitton@cegid.com>

---
+++ packages/core/src/features/preferences/renderer/preference-items/crd/crd-preference-page.injectable.tsx
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) Freelens Authors. All rights reserved.
+ * Copyright (c) OpenLens Authors. All rights reserved.
+ * Licensed under MIT License. See LICENSE in root directory for more information.
+ */
+
+import { getInjectable } from "@ogre-tools/injectable";
+import React from "react";
+import { HorizontalLine } from "../../../../../renderer/components/horizontal-line/horizontal-line";
+import { PreferencePageComponent } from "../../preference-page-component";
+import type { PreferenceItemComponent, PreferencePage } from "../preference-item-injection-token";
+import { preferenceItemInjectionToken } from "../preference-item-injection-token";
+
+const CrdPage: PreferenceItemComponent<PreferencePage> = ({ children, item }) => (
+  <PreferencePageComponent title="Crd" id={item.id}>
+    {children}
+  </PreferencePageComponent>
+);
+
+const crdPreferencePageInjectable = getInjectable({
+  id: "crd-preference-page",
+
+  instantiate: () => ({
+    kind: "page" as const,
+    id: "crd-page",
+    parentId: "crd-tab",
+    Component: CrdPage,
+    childSeparator: () => <HorizontalLine size="sm" />,
+  }),
+
+  injectionToken: preferenceItemInjectionToken,
+});
+
+export default crdPreferencePageInjectable;
