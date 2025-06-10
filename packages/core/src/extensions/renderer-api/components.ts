2521ec00d4eaf8bfa43a1bc80dadc59d353143ce
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2025-06-10 12:24:27 +0200

    Reodered exports

--- packages/core/src/extensions/renderer-api/components.ts
+++ packages/core/src/extensions/renderer-api/components.ts
@@ -17,6 +17,7 @@ import {
   showShortInfoNotificationInjectable,
   showSuccessNotificationInjectable,
 } from "@freelensapp/notifications";
+
 import commandOverlayInjectable from "../../renderer/components/command-palette/command-overlay.injectable";
 import { ConfirmDialog as _ConfirmDialog } from "../../renderer/components/confirm-dialog";
 import type {
@@ -34,47 +35,10 @@ import getDetailsUrlInjectable from "../../renderer/components/kube-detail-param
 import showDetailsInjectable from "../../renderer/components/kube-detail-params/show-details.injectable";
 import podStoreInjectable from "../../renderer/components/workloads-pods/store.injectable";
 
-// layouts
-export * from "../../renderer/components/layout/main-layout";
-export * from "../../renderer/components/layout/setting-layout";
-export * from "../../renderer/components/layout/page-layout";
-export * from "../../renderer/components/layout/wizard-layout";
-export * from "../../renderer/components/layout/tab-layout";
-
-// form-controls
 export * from "@freelensapp/button";
-export * from "../../renderer/components/checkbox";
-export * from "../../renderer/components/radio";
-export * from "../../renderer/components/select";
-export * from "../../renderer/components/slider";
-export * from "../../renderer/components/switch";
-export * from "../../renderer/components/input/input";
-
-// command-overlay
-export const CommandOverlay = asLegacyGlobalForExtensionApi(commandOverlayInjectable);
-
-export type {
-  CategoryColumnRegistration,
-  AdditionalCategoryColumnRegistration,
-} from "../../renderer/components/catalog/custom-category-columns";
-
-// other components
-export type { ConfirmDialogBooleanParams, ConfirmDialogParams, ConfirmDialogProps };
-export const ConfirmDialog = Object.assign(_ConfirmDialog, {
-  open: asLegacyGlobalFunctionForExtensionApi(openConfirmDialogInjectable),
-  confirm: asLegacyGlobalFunctionForExtensionApi(confirmInjectable),
-});
-
 export * from "@freelensapp/icon";
+export * from "@freelensapp/spinner";
 export * from "@freelensapp/tooltip";
-
-export * from "../../renderer/components/tabs";
-export * from "../../renderer/components/table";
-export * from "../../renderer/components/badge";
-export * from "../../renderer/components/drawer";
-export * from "../../renderer/components/dialog";
-export * from "../../renderer/components/line-progress";
-export * from "../../renderer/components/menu";
 
 export {
   NotificationStatus,
@@ -86,26 +50,52 @@ export {
   type NotificationsStore,
 } from "@freelensapp/notifications";
 
-export const Notifications = {
-  ok: asLegacyGlobalFunctionForExtensionApi(showSuccessNotificationInjectable),
-  error: asLegacyGlobalFunctionForExtensionApi(showErrorNotificationInjectable),
-  checkedError: asLegacyGlobalFunctionForExtensionApi(showCheckedErrorNotificationInjectable),
-  info: asLegacyGlobalFunctionForExtensionApi(showInfoNotificationInjectable),
-  shortInfo: asLegacyGlobalFunctionForExtensionApi(showShortInfoNotificationInjectable),
-};
-
-export * from "@freelensapp/spinner";
-
-export * from "../../renderer/components/stepper";
-export * from "../../renderer/components/wizard";
-export * from "../../renderer/components/workloads-pods/pod-details-list";
-export * from "../../renderer/components/namespaces/namespace-select";
-export * from "../../renderer/components/namespaces/namespace-select-filter";
-export * from "../../renderer/components/layout/sub-title";
-export * from "../../renderer/components/input/search-input";
+export * from "../../renderer/components/add-remove-buttons";
+export * from "../../renderer/components/avatar";
+export * from "../../renderer/components/badge";
 export * from "../../renderer/components/chart/bar-chart";
 export * from "../../renderer/components/chart/pie-chart";
+export * from "../../renderer/components/checkbox";
+export * from "../../renderer/components/countdown";
+export * from "../../renderer/components/dialog";
+export * from "../../renderer/components/drawer";
+export * from "../../renderer/components/duration";
+export * from "../../renderer/components/events/kube-event-details";
+export * from "../../renderer/components/input/input";
+export * from "../../renderer/components/input/search-input";
+export * from "../../renderer/components/kube-object-details";
+export * from "../../renderer/components/kube-object-list-layout";
+export * from "../../renderer/components/kube-object-menu";
+export * from "../../renderer/components/kube-object-meta";
+export * from "../../renderer/components/kube-object";
+export * from "../../renderer/components/layout/main-layout";
+export * from "../../renderer/components/layout/page-layout";
+export * from "../../renderer/components/layout/setting-layout";
+export * from "../../renderer/components/layout/sub-title";
+export * from "../../renderer/components/layout/tab-layout";
+export * from "../../renderer/components/layout/wizard-layout";
+export * from "../../renderer/components/line-progress";
 export * from "../../renderer/components/markdown-viewer";
+export * from "../../renderer/components/menu";
+export * from "../../renderer/components/namespaces/namespace-select-filter";
+export * from "../../renderer/components/namespaces/namespace-select";
+export * from "../../renderer/components/radio";
+export * from "../../renderer/components/resource-metrics/resource-metrics";
+export * from "../../renderer/components/select";
+export * from "../../renderer/components/slider";
+export * from "../../renderer/components/status-brick";
+export * from "../../renderer/components/stepper";
+export * from "../../renderer/components/switch";
+export * from "../../renderer/components/table";
+export * from "../../renderer/components/tabs";
+export * from "../../renderer/components/wizard";
+export * from "../../renderer/components/workloads-pods/pod-charts";
+export * from "../../renderer/components/workloads-pods/pod-details-list";
+
+export type {
+  CategoryColumnRegistration,
+  AdditionalCategoryColumnRegistration,
+} from "../../renderer/components/catalog/custom-category-columns";
 
 export {
   MonacoEditor,
@@ -115,8 +105,21 @@ export {
   type MonacoCustomTheme,
 } from "../../renderer/components/monaco-editor";
 
-export * from "../../renderer/components/resource-metrics/resource-metrics";
-export * from "../../renderer/components/workloads-pods/pod-charts";
+export const CommandOverlay = asLegacyGlobalForExtensionApi(commandOverlayInjectable);
+
+export type { ConfirmDialogBooleanParams, ConfirmDialogParams, ConfirmDialogProps };
+export const ConfirmDialog = Object.assign(_ConfirmDialog, {
+  open: asLegacyGlobalFunctionForExtensionApi(openConfirmDialogInjectable),
+  confirm: asLegacyGlobalFunctionForExtensionApi(confirmInjectable),
+});
+
+export const Notifications = {
+  ok: asLegacyGlobalFunctionForExtensionApi(showSuccessNotificationInjectable),
+  error: asLegacyGlobalFunctionForExtensionApi(showErrorNotificationInjectable),
+  checkedError: asLegacyGlobalFunctionForExtensionApi(showCheckedErrorNotificationInjectable),
+  info: asLegacyGlobalFunctionForExtensionApi(showInfoNotificationInjectable),
+  shortInfo: asLegacyGlobalFunctionForExtensionApi(showShortInfoNotificationInjectable),
+};
 
 /**
  * @deprecated Use `Renderer.Navigation.getDetailsUrl`
@@ -127,21 +130,6 @@ export const getDetailsUrl = asLegacyGlobalFunctionForExtensionApi(getDetailsUrl
  * @deprecated Use `Renderer.Navigation.showDetails`
  */
 export const showDetails = asLegacyGlobalFunctionForExtensionApi(showDetailsInjectable);
-
-// kube helpers
-export * from "../../renderer/components/kube-object";
-export * from "../../renderer/components/kube-object-details";
-export * from "../../renderer/components/kube-object-list-layout";
-export * from "../../renderer/components/kube-object-menu";
-export * from "../../renderer/components/kube-object-meta";
-export * from "../../renderer/components/events/kube-event-details";
-
-// specific exports
-export * from "../../renderer/components/add-remove-buttons";
-export * from "../../renderer/components/avatar";
-export * from "../../renderer/components/countdown";
-export * from "../../renderer/components/duration";
-export * from "../../renderer/components/status-brick";
 
 export const createTerminalTab = asLegacyGlobalFunctionForExtensionApi(createTerminalTabInjectable);
 
