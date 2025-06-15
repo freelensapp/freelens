e33bde047dd84bc71ecc65955f3b3550920fc165
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2025-06-16 01:55:29 +0200

    Expose ReactJsxRuntime for Extensions

--- packages/core/src/renderer/extension-api.ts
+++ packages/core/src/renderer/extension-api.ts
@@ -11,6 +11,7 @@ export { React, ReactDOM };
 
 export * as Mobx from "mobx";
 export * as MobxReact from "mobx-react";
+export * as ReactJsxRuntime from "react/jsx-runtime";
 export * as ReactRouter from "react-router";
 export * as ReactRouterDom from "react-router-dom";
 export * as RendererApi from "../extensions/renderer-api";
