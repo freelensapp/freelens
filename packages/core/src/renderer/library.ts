dedf74df44ca77340472933d3ee764d7950501f0
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2025-06-16 02:52:19 +0200

    Expose ReactJsxRuntime for Extensions (#858)

--- packages/core/src/renderer/library.ts
+++ packages/core/src/renderer/library.ts
@@ -15,6 +15,7 @@ export { registerLensCore } from "./register-lens-core";
 export { React, ReactDOM, setStatusBarStatusInjectable };
 export * as Mobx from "mobx";
 export * as MobxReact from "mobx-react";
+export * as ReactJsxRuntime from "react/jsx-runtime";
 export * as ReactRouter from "react-router";
 export * as ReactRouterDom from "react-router-dom";
 export * as rendererExtensionApi from "../extensions/renderer-api";
