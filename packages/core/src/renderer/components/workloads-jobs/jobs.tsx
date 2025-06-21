0b99b68d139bc0a35010cbf3cb365caf22a30def
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2025-06-21 09:40:24 +0200

    Jobs view redesigned

--- packages/core/src/renderer/components/workloads-jobs/jobs.tsx
+++ packages/core/src/renderer/components/workloads-jobs/jobs.tsx
@@ -6,10 +6,11 @@
 
 import "./jobs.scss";
 
+import { formatDuration } from "@freelensapp/utilities/dist";
 import { withInjectables } from "@ogre-tools/injectable-react";
-import kebabCase from "lodash/kebabCase";
 import { observer } from "mobx-react";
 import React from "react";
+import { Badge, WithTooltip } from "../badge";
 import eventStoreInjectable from "../events/store.injectable";
 import { KubeObjectAge } from "../kube-object/age";
 import { KubeObjectListLayout } from "../kube-object-list-layout";
@@ -18,15 +19,58 @@ import { SiblingsInTabLayout } from "../layout/siblings-in-tab-layout";
 import { NamespaceSelectBadge } from "../namespaces/namespace-select-badge";
 import jobStoreInjectable from "./store.injectable";
 
+import type { Job } from "@freelensapp/kube-object";
+
 import type { EventStore } from "../events/store";
 import type { JobStore } from "./store";
 
 enum columnId {
   name = "name",
   namespace = "namespace",
+  status = "status",
+  succeeded = "succeeded",
   completions = "completions",
-  conditions = "conditions",
+  parallelism = "parallelism",
+  duration = "duration",
   age = "age",
+}
+
+export function getStatusText(obj: Job) {
+  const conditions = obj.getConditions();
+  if (!conditions || !conditions.length) {
+    return "Unknown";
+  }
+  if (obj.hasCondition("Complete")) {
+    return "Complete";
+  } else if (obj.hasCondition("Failed")) {
+    return "Failed";
+  } else if (obj.metadata.deletionTimestamp) {
+    return "Terminating";
+  } else if (obj.hasCondition("Suspended")) {
+    return "Suspended";
+  } else if (obj.hasCondition("FailureTarget")) {
+    return "FailureTarget";
+  }
+  return "Running";
+}
+
+export type JobStatus = ReturnType<typeof getStatusText>;
+
+export function getStatusClass(obj: Job) {
+  const status = getStatusText(obj);
+  switch (status) {
+    case "Complete":
+      return "success";
+    case "Failed":
+    case "FailureTarget":
+      return "error";
+    case "Running":
+      return "info";
+    case "Suspended":
+      return "warning";
+    default:
+      return "";
+  }
 }
 
 interface Dependencies {
@@ -48,7 +92,11 @@ const NonInjectedJobs = observer((props: Dependencies) => {
         sortingCallbacks={{
           [columnId.name]: (job) => job.getName(),
           [columnId.namespace]: (job) => job.getNs(),
-          [columnId.conditions]: (job) => job.getCondition()?.type,
+          [columnId.succeeded]: (job) => job.getCompletions(),
+          [columnId.completions]: (job) => job.getDesiredCompletions(),
+          [columnId.parallelism]: (job) => job.getParallelism(),
+          [columnId.status]: (job) => getStatusText(job),
+          [columnId.duration]: (job) => job.getJobDuration(),
           [columnId.age]: (job) => -job.getCreationTimestamp(),
         }}
         searchFilters={[(job) => job.getSearchFields()]}
@@ -56,24 +104,25 @@ const NonInjectedJobs = observer((props: Dependencies) => {
         renderTableHeader={[
           { title: "Name", className: "name", sortBy: columnId.name, id: columnId.name },
           { title: "Namespace", className: "namespace", sortBy: columnId.namespace, id: columnId.namespace },
-          { title: "Completions", className: "completions", id: columnId.completions },
-          { className: "warning", showWithColumn: columnId.completions },
+          { title: "Status", className: "status", sortBy: columnId.status, id: columnId.status },
+          { className: "warning", showWithColumn: columnId.succeeded },
+          { title: "Succeeded", className: "succeeded", sortBy: columnId.succeeded, id: columnId.succeeded },
+          { title: "Completions", className: "completions", sortBy: columnId.completions, id: columnId.completions },
+          { title: "Parallelism", className: "parallelism", sortBy: columnId.parallelism, id: columnId.parallelism },
+          { title: "Duration", className: "duration", sortBy: columnId.duration, id: columnId.duration },
           { title: "Age", className: "age", sortBy: columnId.age, id: columnId.age },
-          { title: "Conditions", className: "conditions", sortBy: columnId.conditions, id: columnId.conditions },
         ]}
         renderTableContents={(job) => {
-          const condition = job.getCondition();
-
           return [
-            job.getName(),
+            <WithTooltip>{job.getName()}</WithTooltip>,
             <NamespaceSelectBadge key="namespace" namespace={job.getNs()} />,
-            `${job.getCompletions()} / ${job.getDesiredCompletions()}`,
+            <Badge className={getStatusClass(job)} label={getStatusText(job)} tooltip={getStatusText(job)} />,
             <KubeObjectStatusIcon key="icon" object={job} />,
+            job.getCompletions(),
+            job.getDesiredCompletions(),
+            job.getParallelism(),
+            formatDuration(job.getJobDuration()),
             <KubeObjectAge key="age" object={job} />,
-            condition && {
-              title: condition.type,
-              className: kebabCase(condition.type),
-            },
           ];
         }}
       />
