8f6537cd631e0b8ec20f151c498eef4bbbc98aae
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2025-06-22 20:29:50 +0200

    Labels column removed from Secrets view

--- packages/core/src/renderer/components/config-secrets/secrets.tsx
+++ packages/core/src/renderer/components/config-secrets/secrets.tsx
@@ -9,7 +9,6 @@ import "./secrets.scss";
 import { withInjectables } from "@ogre-tools/injectable-react";
 import { observer } from "mobx-react";
 import React from "react";
-import { Badge } from "../badge";
 import { KubeObjectAge } from "../kube-object/age";
 import { KubeObjectListLayout } from "../kube-object-list-layout";
 import { KubeObjectStatusIcon } from "../kube-object-status-icon";
@@ -49,7 +48,6 @@ class NonInjectedSecrets extends React.Component<Dependencies> {
           sortingCallbacks={{
             [columnId.name]: (secret) => secret.getName(),
             [columnId.namespace]: (secret) => secret.getNs(),
-            [columnId.labels]: (secret) => secret.getLabels(),
             [columnId.keys]: (secret) => secret.getKeys(),
             [columnId.type]: (secret) => secret.type,
             [columnId.age]: (secret) => -secret.getCreationTimestamp(),
@@ -60,7 +58,6 @@ class NonInjectedSecrets extends React.Component<Dependencies> {
             { title: "Name", className: "name", sortBy: columnId.name, id: columnId.name },
             { className: "warning", showWithColumn: columnId.name },
             { title: "Namespace", className: "namespace", sortBy: columnId.namespace, id: columnId.namespace },
-            { title: "Labels", className: "labels scrollable", sortBy: columnId.labels, id: columnId.labels },
             { title: "Keys", className: "keys", sortBy: columnId.keys, id: columnId.keys },
             { title: "Type", className: "type", sortBy: columnId.type, id: columnId.type },
             { title: "Age", className: "age", sortBy: columnId.age, id: columnId.age },
@@ -69,11 +66,6 @@ class NonInjectedSecrets extends React.Component<Dependencies> {
             <WithTooltip>{secret.getName()}</WithTooltip>,
             <KubeObjectStatusIcon key="icon" object={secret} />,
             <NamespaceSelectBadge key="namespace" namespace={secret.getNs()} />,
-            <WithTooltip>
-              {secret.getLabels().map((label) => (
-                <Badge scrollable key={label} label={label} expandable={false} />
-              ))}
-            </WithTooltip>,
             <WithTooltip>{secret.getKeys().join(", ")}</WithTooltip>,
             <WithTooltip>{secret.type}</WithTooltip>,
             <KubeObjectAge key="age" object={secret} />,
