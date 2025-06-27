078968bf97c6cb28de71681acc043e17cc7eb9a8
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2025-06-27 17:54:13 +0200

    pnpm manager used for installing extensions use application directory for its files (#920)

--- packages/core/src/extensions/install-extension/install-extension.injectable.ts
+++ packages/core/src/extensions/install-extension/install-extension.injectable.ts
@@ -10,6 +10,7 @@ import { getInjectable } from "@ogre-tools/injectable";
 import AwaitLock from "await-lock";
 import { fork } from "child_process";
 import { once } from "lodash";
+import directoryForUserDataInjectable from "../../common/app-paths/directory-for-user-data/directory-for-user-data.injectable";
 import pathToPnpmCliInjectable from "../../common/app-paths/path-to-pnpm-cli.injectable";
 import readJsonFileInjectable from "../../common/fs/read-json-file.injectable";
 import writeJsonFileInjectable from "../../common/fs/write-json-file.injectable";
@@ -25,6 +26,7 @@ export type InstallExtension = (name: string) => Promise<void>;
 const installExtensionInjectable = getInjectable({
   id: "install-extension",
   instantiate: (di): InstallExtension => {
+    const directoryForUserData = di.inject(directoryForUserDataInjectable);
     const pathToPnpmCli = di.inject(pathToPnpmCliInjectable);
     const extensionPackageRootDirectory = di.inject(extensionPackageRootDirectoryInjectable);
     const readJsonFile = di.inject(readJsonFileInjectable);
@@ -32,12 +34,23 @@ const installExtensionInjectable = getInjectable({
     const joinPaths = di.inject(joinPathsInjectable);
     const logger = di.inject(prefixedLoggerInjectable, "EXTENSION-INSTALLER");
 
+    const pnpmHome = joinPaths(directoryForUserData, "pnpm", "data");
+    const cacheDir = joinPaths(directoryForUserData, "cache");
+    const configDir = joinPaths(directoryForUserData, "config");
+    const stateDir = joinPaths(directoryForUserData, "state");
+
     const forkPnpm = (...args: string[]) =>
       new Promise<void>((resolve, reject) => {
         const child = fork(pathToPnpmCli, args, {
           cwd: extensionPackageRootDirectory,
           silent: false,
-          env: process.env,
+          env: {
+            ...process.env,
+            PNPM_HOME: pnpmHome,
+            XDG_CACHE_HOME: cacheDir,
+            XDG_CONFIG_HOME: configDir,
+            XDG_STATE_HOME: stateDir,
+          },
         });
         let stdout = "";
         let stderr = "";
