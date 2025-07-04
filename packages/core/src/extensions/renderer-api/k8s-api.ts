5ec037e8797141fe0a3d648716c95475f7e3691f
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2025-07-05 00:54:09 +0200

    export stores

--- packages/core/src/extensions/renderer-api/k8s-api.ts
+++ packages/core/src/extensions/renderer-api/k8s-api.ts
@@ -13,6 +13,38 @@ import {
 import requestMetricsInjectable from "../../common/k8s-api/endpoints/metrics.api/request-metrics.injectable";
 import { apiResourceRecord } from "../../common/rbac";
 import { shouldShowResourceInjectionToken } from "../../features/cluster/showing-kube-resources/common/allowed-resources-injection-token";
+import horizontalPodAutoscalerStoreInjectable from "../../renderer/components/config-horizontal-pod-autoscalers/store.injectable";
+import limitRangeStoreInjectable from "../../renderer/components/config-limit-ranges/store.injectable";
+import configMapStoreInjectable from "../../renderer/components/config-maps/store.injectable";
+import podDisruptionBudgetStoreInjectable from "../../renderer/components/config-pod-disruption-budgets/store.injectable";
+import priorityClassStoreInjectable from "../../renderer/components/config-priority-classes/store.injectable";
+import resourceQuotaStoreInjectable from "../../renderer/components/config-resource-quotas/store.injectable";
+import secretStoreInjectable from "../../renderer/components/config-secrets/store.injectable";
+import verticalPodAutoscalerStoreInjectable from "../../renderer/components/config-vertical-pod-autoscalers/store.injectable";
+import customResourceDefinitionStoreInjectable from "../../renderer/components/custom-resource-definitions/store.injectable";
+import eventStoreInjectable from "../../renderer/components/events/store.injectable";
+import namespaceStoreInjectable from "../../renderer/components/namespaces/store.injectable";
+import endpointSliceStoreInjectable from "../../renderer/components/network-endpoint-slices/store.injectable";
+import endpointsStoreInjectable from "../../renderer/components/network-endpoints/store.injectable";
+import ingressStoreInjectable from "../../renderer/components/network-ingresses/ingress-store.injectable";
+import networkPolicyStoreInjectable from "../../renderer/components/network-policies/store.injectable";
+import serviceStoreInjectable from "../../renderer/components/network-services/store.injectable";
+import nodeStoreInjectable from "../../renderer/components/nodes/store.injectable";
+import storageClassStoreInjectable from "../../renderer/components/storage-classes/store.injectable";
+import persistentVolumeClaimStoreInjectable from "../../renderer/components/storage-volume-claims/store.injectable";
+import persistentVolumeStoreInjectable from "../../renderer/components/storage-volumes/store.injectable";
+import clusterRoleBindingStoreInjectable from "../../renderer/components/user-management/cluster-role-bindings/store.injectable";
+import clusterRoleStoreInjectable from "../../renderer/components/user-management/cluster-roles/store.injectable";
+import roleBindingStoreInjectable from "../../renderer/components/user-management/role-bindings/store.injectable";
+import roleStoreInjectable from "../../renderer/components/user-management/roles/store.injectable";
+import serviceAccountStoreInjectable from "../../renderer/components/user-management/service-accounts/store.injectable";
+import cronJobStoreInjectable from "../../renderer/components/workloads-cronjobs/store.injectable";
+import daemonSetStoreInjectable from "../../renderer/components/workloads-daemonsets/store.injectable";
+import deploymentStoreInjectable from "../../renderer/components/workloads-deployments/store.injectable";
+import jobStoreInjectable from "../../renderer/components/workloads-jobs/store.injectable";
+import podStoreInjectable from "../../renderer/components/workloads-pods/store.injectable";
+import replicaSetStoreInjectable from "../../renderer/components/workloads-replicasets/store.injectable";
+import statefulSetStoreInjectable from "../../renderer/components/workloads-statefulsets/store.injectable";
 
 import type { KubeResource } from "../../common/rbac";
 
@@ -68,6 +100,39 @@ export const serviceApi = asLegacyGlobalForExtensionApi(kubeApiSpecifics.service
 export const statefulSetApi = asLegacyGlobalForExtensionApi(kubeApiSpecifics.statefulSetApiInjectable);
 export const storageClassApi = asLegacyGlobalForExtensionApi(kubeApiSpecifics.storageClassApiInjectable);
 export const vpaApi = asLegacyGlobalForExtensionApi(kubeApiSpecifics.verticalPodAutoscalerApiInjectable);
+
+export const clusterRoleStore = asLegacyGlobalForExtensionApi(clusterRoleStoreInjectable);
+export const clusterRoleBindingStore = asLegacyGlobalForExtensionApi(clusterRoleBindingStoreInjectable);
+export const configMapStore = asLegacyGlobalForExtensionApi(configMapStoreInjectable);
+export const crdStore = asLegacyGlobalForExtensionApi(customResourceDefinitionStoreInjectable);
+export const cronJobStore = asLegacyGlobalForExtensionApi(cronJobStoreInjectable);
+export const daemonSetStore = asLegacyGlobalForExtensionApi(daemonSetStoreInjectable);
+export const deploymentStore = asLegacyGlobalForExtensionApi(deploymentStoreInjectable);
+export const endpointStore = asLegacyGlobalForExtensionApi(endpointsStoreInjectable);
+export const endpointSliceStore = asLegacyGlobalForExtensionApi(endpointSliceStoreInjectable);
+export const eventStore = asLegacyGlobalForExtensionApi(eventStoreInjectable);
+export const hpaStore = asLegacyGlobalForExtensionApi(horizontalPodAutoscalerStoreInjectable);
+export const ingressStore = asLegacyGlobalForExtensionApi(ingressStoreInjectable);
+export const jobStore = asLegacyGlobalForExtensionApi(jobStoreInjectable);
+export const limitRangeStore = asLegacyGlobalForExtensionApi(limitRangeStoreInjectable);
+export const namespaceStore = asLegacyGlobalForExtensionApi(namespaceStoreInjectable);
+export const networkPolicyStore = asLegacyGlobalForExtensionApi(networkPolicyStoreInjectable);
+export const nodesStore = asLegacyGlobalForExtensionApi(nodeStoreInjectable);
+export const pcStore = asLegacyGlobalForExtensionApi(priorityClassStoreInjectable);
+export const pdbStore = asLegacyGlobalForExtensionApi(podDisruptionBudgetStoreInjectable);
+export const persistentVolumeStore = asLegacyGlobalForExtensionApi(persistentVolumeStoreInjectable);
+export const podsStore = asLegacyGlobalForExtensionApi(podStoreInjectable);
+export const pvcStore = asLegacyGlobalForExtensionApi(persistentVolumeClaimStoreInjectable);
+export const replicaSetStore = asLegacyGlobalForExtensionApi(replicaSetStoreInjectable);
+export const resourceQuotaStore = asLegacyGlobalForExtensionApi(resourceQuotaStoreInjectable);
+export const roleStore = asLegacyGlobalForExtensionApi(roleStoreInjectable);
+export const roleBindingStore = asLegacyGlobalForExtensionApi(roleBindingStoreInjectable);
+export const secretsStore = asLegacyGlobalForExtensionApi(secretStoreInjectable);
+export const serviceAccountsStore = asLegacyGlobalForExtensionApi(serviceAccountStoreInjectable);
+export const serviceStore = asLegacyGlobalForExtensionApi(serviceStoreInjectable);
+export const statefulSetStore = asLegacyGlobalForExtensionApi(statefulSetStoreInjectable);
+export const storageClassStore = asLegacyGlobalForExtensionApi(storageClassStoreInjectable);
+export const vpaStore = asLegacyGlobalForExtensionApi(verticalPodAutoscalerStoreInjectable);
 
 export * from "../common-api/k8s-api";
 
