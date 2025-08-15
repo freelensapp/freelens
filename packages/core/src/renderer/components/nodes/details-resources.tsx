57be4d02023c75af6598e14100634cb6c2855e62
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2025-08-15 21:23:55 +0200

    Show all resources on nodes (#1098)

--- packages/core/src/renderer/components/nodes/details-resources.tsx
+++ packages/core/src/renderer/components/nodes/details-resources.tsx
@@ -4,27 +4,16 @@
  * Licensed under MIT License. See LICENSE in root directory for more information.
  */
 
-import "./details-resources.scss";
-
+import { bytesToUnits, unitsToBytes } from "@freelensapp/utilities";
 import React from "react";
-import { Table } from "../table/table";
-import { TableCell } from "../table/table-cell";
-import { TableHead } from "../table/table-head";
-import { TableRow } from "../table/table-row";
+import { DrawerItem } from "../drawer";
+import { WithTooltip } from "../with-tooltip";
 
 import type { Node } from "@freelensapp/kube-object";
 
 export interface NodeDetailsResourcesProps {
   node: Node;
   type: "allocatable" | "capacity";
-}
-
-function toMi(resource: string | undefined) {
-  if (resource?.endsWith("Ki")) {
-    return `${(parseInt(resource) / 1024).toFixed(1)}Mi`;
-  }
-
-  return resource;
 }
 
 export function NodeDetailsResources({ type, node: { status = {} } }: NodeDetailsResourcesProps) {
@@ -35,25 +24,26 @@ export function NodeDetailsResources({ type, node: { status = {} } }: NodeDetail
   }
 
   return (
-    <div className="NodeDetailsResources flex column">
-      <Table selectable scrollable={false}>
-        <TableHead sticky={false} flat>
-          <TableCell className="cpu">CPU</TableCell>
-          <TableCell className="memory">Memory</TableCell>
-          <TableCell className="ephemeral-storage">Ephemeral Storage</TableCell>
-          <TableCell className="hugepages-1Gi">Hugepages-1Gi</TableCell>
-          <TableCell className="hugepages-2Mi">Hugepages-2Mi</TableCell>
-          <TableCell className="pods">Pods</TableCell>
-        </TableHead>
-        <TableRow>
-          <TableCell className="cpu">{resourceStatus.cpu}</TableCell>
-          <TableCell className="memory">{toMi(resourceStatus.memory)}</TableCell>
-          <TableCell className="ephemeral-storage">{toMi(resourceStatus["ephemeral-storage"])}</TableCell>
-          <TableCell className="hugepages-1Gi">{resourceStatus["hugepages-1Gi"]}</TableCell>
-          <TableCell className="hugepages-2Mi">{resourceStatus["hugepages-2Mi"]}</TableCell>
-          <TableCell className="pods">{resourceStatus.pods}</TableCell>
-        </TableRow>
-      </Table>
+    <div className="NodeDetailsResources">
+      {Object.entries(resourceStatus).map(([key, value]) => {
+        let tooltip = null;
+        if (value === undefined) return null;
+        if (value === null) return null;
+        if (key === "ephemeral-storage" || key === "memory") {
+          const newValue = bytesToUnits(unitsToBytes(value));
+          if (newValue !== "N/A") {
+            tooltip = value;
+            value = newValue;
+          }
+        }
+        return (
+          <DrawerItem key={key} name={key}>
+            <WithTooltip tooltip={tooltip}>
+              <span>{value}</span>
+            </WithTooltip>
+          </DrawerItem>
+        );
+      })}
     </div>
   );
 }
