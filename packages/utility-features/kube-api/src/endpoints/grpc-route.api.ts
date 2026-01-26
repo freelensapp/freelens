ebc4dc6cce5617df23e257bdb5156938ee06b7fa
omarluq <omar.luqman@hey.com>
2026-01-26 01:38:43 -0600

    feat(gateway-api): add KubeApi wrappers for Gateway API resources

---
+++ packages/utility-features/kube-api/src/endpoints/grpc-route.api.ts
@@ -0,0 +1,18 @@
+/**
+ * Copyright (c) Freelens Authors. All rights reserved.
+ * Licensed under MIT License. See LICENSE in root directory for more information.
+ */
+
+import { GRPCRoute } from "@freelensapp/kube-object";
+import { KubeApi } from "../kube-api";
+
+import type { DerivedKubeApiOptions, KubeApiDependencies } from "../kube-api";
+
+export class GRPCRouteApi extends KubeApi<GRPCRoute> {
+  constructor(deps: KubeApiDependencies, opts?: DerivedKubeApiOptions) {
+    super(deps, {
+      ...(opts ?? {}),
+      objectConstructor: GRPCRoute,
+    });
+  }
+}
