67c8d7912961b50ca7b5051f8c8c5d1a47863ecb
MatiasRoje <rojechi@gmail.com>
2026-02-03 18:46:50 +0100

    style favorites overview with SCSS modules

--- packages/core/src/renderer/components/favorites/overview/overview.tsx
+++ packages/core/src/renderer/components/favorites/overview/overview.tsx
@@ -4,12 +4,14 @@
  * Licensed under MIT License. See LICENSE in root directory for more information.
  */
 
-import "./overview.scss";
+import styles from "./overview.module.scss";
 
 import React from "react";
 import { observer } from "mobx-react";
+import { Icon } from "@freelensapp/icon";
+import { cssNames } from "@freelensapp/utilities";
 
-import { SiblingsInTabLayout } from "../../layout/siblings-in-tab-layout";
+import { TabLayout } from "../../layout/tab-layout";
 import favoritesStoreInjectable, { FavoritesStore } from "../store.injectable";
 import favoritesSidebarItemsComputedInjectable, { FavoriteSidebarItem } from "../sidebar-items-computed.injectable";
 import type { IComputedValue } from "mobx";
@@ -23,37 +25,82 @@ interface Dependencies {
 }
 
 const NonInjectedFavoritesOverview = observer(({ favoritesStore, favoritesSidebarItems }: Dependencies) => {
+  const favorites = favoritesStore.items;
+  const hasFavorites = favorites.length > 0;
+
+  const handleRemove = (itemId: string) => {
+    favoritesStore.remove(itemId);
+  };
+
   const renderItem = (item: FavoriteItem) => {
     const computedItem = favoritesSidebarItems.get().find((i) => i.id.replace("favorite-", "") === item.id);
+    const isAvailable = !!computedItem;
 
-    if (!computedItem) {
-      return (
-        <div key={item.id}>
-          <span>{item.title}</span>
-          <span>Unavailable</span>
-        </div>
-      );
-    }
+    const handleCardClick = () => {
+      if (isAvailable && computedItem) {
+        computedItem.onClick();
+      }
+    };
 
     return (
-      <div key={item.id}>
-        <span>{computedItem.title}</span>
-        <span>Available</span>
+      <div
+        key={item.id}
+        className={cssNames(styles.favoriteCard, { [styles.unavailable]: !isAvailable })}
+        onClick={handleCardClick}
+      >
+        <div className={styles.dragHandle}>
+          <Icon material="drag_indicator" />
+        </div>
+        <div className={styles.content}>
+          <div className={styles.title}>{item.title}</div>
+        </div>
+        <button
+          className={styles.removeButton}
+          onClick={(e) => {
+            e.stopPropagation();
+            handleRemove(item.id);
+          }}
+          title="Remove from favorites"
+        >
+          <Icon material="close" />
+        </button>
       </div>
     );
   };
 
   return (
-    <SiblingsInTabLayout>
-      <div className="FavoritesOverview">
-        <div>
-          <h5>Your Favorites</h5>
-          <OrderableList className={``} onReorder={(start, end) => favoritesStore.reorder(start, end)}>
-            {favoritesStore.items.map((item) => renderItem(item))}
-          </OrderableList>
+    <TabLayout scrollable>
+      <div className={styles.FavoritesOverview}>
+        <div className={styles.favoritesContainer}>
+          <div className={styles.header}>
+            <h5>Favorites</h5>
+          </div>
+
+          {!hasFavorites ? (
+            <div className={styles.emptyState}>
+              <Icon material="star_border" className={styles.emptyIcon} />
+              <h6>No favorites yet</h6>
+              <p>
+                Start by hovering over any item in the sidebar and clicking the pin icon to add it here. Your favorites
+                will be saved and easily accessible from this dashboard.
+              </p>
+            </div>
+          ) : (
+            <>
+              <div className={styles.countLabel}>
+                {favorites.length} {favorites.length === 1 ? "item" : "items"}
+              </div>
+              <OrderableList
+                className={styles.favoritesList}
+                onReorder={(start, end) => favoritesStore.reorder(start, end)}
+              >
+                {favorites.map((item) => renderItem(item))}
+              </OrderableList>
+            </>
+          )}
         </div>
       </div>
-    </SiblingsInTabLayout>
+    </TabLayout>
   );
 });
 
