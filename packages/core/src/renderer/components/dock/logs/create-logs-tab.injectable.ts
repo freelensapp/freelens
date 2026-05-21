5f63bfd21002e2bdb68deba80747dd6b34324656
MatiasRoje <rojechi@gmail.com>
2026-05-21 19:05:15 +0200

    add user preferences integration to create-logs-tab

--- packages/core/src/renderer/components/dock/logs/create-logs-tab.injectable.ts
+++ packages/core/src/renderer/components/dock/logs/create-logs-tab.injectable.ts
@@ -6,6 +6,8 @@
 
 import { getInjectable } from "@ogre-tools/injectable";
 import { runInAction } from "mobx";
+import { defaultLogViewerPreferences } from "../../../../features/user-preferences/common/preferences-helpers";
+import userPreferencesStateInjectable from "../../../../features/user-preferences/common/state.injectable";
 import createDockTabInjectable from "../dock/create-dock-tab.injectable";
 import { TabKind } from "../dock/store";
 import getRandomIdForPodLogsTabInjectable from "./get-random-id-for-pod-logs-tab.injectable";
@@ -13,6 +15,7 @@ import setLogTabDataInjectable from "./set-log-tab-data.injectable";
 
 import type { DockTab, DockTabCreate, TabId } from "../dock/store";
 import type { LogTabData } from "./tab-store";
+import type { UserPreferencesState } from "../../../../features/user-preferences/common/state.injectable";
 
 export type CreateLogsTabData = Pick<LogTabData, "owner" | "selectedPodId" | "selectedContainer" | "namespace"> &
   Omit<Partial<LogTabData>, "owner" | "selectedPodId" | "selectedContainer" | "namespace">;
@@ -21,10 +24,11 @@ interface Dependencies {
   createDockTab: (rawTabDesc: DockTabCreate, addNumber?: boolean) => DockTab;
   setLogTabData: (tabId: string, data: LogTabData) => void;
   getRandomId: () => string;
+  userPreferencesState: UserPreferencesState;
 }
 
 const createLogsTab =
-  ({ createDockTab, setLogTabData, getRandomId }: Dependencies) =>
+  ({ createDockTab, setLogTabData, getRandomId, userPreferencesState }: Dependencies) =>
   (title: string, data: CreateLogsTabData): TabId => {
     const id = `log-tab-${getRandomId()}`;
 
@@ -38,9 +42,7 @@ const createLogsTab =
         false,
       );
       setLogTabData(id, {
-        showTimestamps: false,
-        showPrevious: false,
-        showWordWrap: true,
+        ...(userPreferencesState.logViewerPreferences ?? defaultLogViewerPreferences),
         ...data,
       });
     });
@@ -56,6 +58,7 @@ const createLogsTabInjectable = getInjectable({
       createDockTab: di.inject(createDockTabInjectable),
       setLogTabData: di.inject(setLogTabDataInjectable),
       getRandomId: di.inject(getRandomIdForPodLogsTabInjectable),
+      userPreferencesState: di.inject(userPreferencesStateInjectable),
     }),
 });
 
